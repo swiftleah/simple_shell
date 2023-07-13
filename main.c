fb7b9a2c9cc3109fab336c871148c6985ce02d94
swiftleah <leahrosehenry@gmail.com>
2023-07-13 01:23:35 -0700

    Removed while(1) condition in main.c file

--- main.c
+++ main.c
@@ -11,27 +11,24 @@ int main(void)
 	size_t bufsize = 0;
 	ssize_t input;
 
-	while (1)
-	{
-		displayprompt();
-		input = getline(&line, &bufsize, stdin);
+	displayprompt();
+	input = getline(&line, &bufsize, stdin);
 
-		if (input == -1)
+	if (input == -1)
+	{
+		if (line == NULL || (line[0] == '\0' || line[0] == '\n'))
 		{
-			if (line == NULL || (line[0] == '\0' || line[0] == '\n'))
-			{
-				printf("End of file reached.\n");
-			}
-			else
-			{
-				perror("Error: End of file reached\n");
-			}
-			free(line);
-			return (1);
+			printf("End of file reached.\n");
 		}
-	}
-		parseinput(line, bufsize);
+		else
+		{
+			perror("Error: End of file reached");
+		}
 		free(line);
-		return (0);
+		return (1);
+	}
+	parseinput(line, bufsize);
+	free(line);
+	return (0);
 }
 
