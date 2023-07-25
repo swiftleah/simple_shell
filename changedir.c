9fcf5946350748f9f136b5a241dae1994e0a0bee
swiftleah <leahrosehenry@gmail.com>
2023-07-25 04:41:37 -0700

    Fixed betty errors for changedir func

--- changedir.c
+++ changedir.c
@@ -10,46 +10,41 @@ int change_dir(char **args)
 	char *path = args[1];
 	char cwd[1024];
 
-	    if (path == NULL)
-	    {
-		    path = getenv("HOME");
-		    if (path == NULL)
-		    {
-			    printf("HOME environment variable not set.\n");
-			    return 1;
-		    }
-	    }
-	    else if (strcmp(path, "-") == 0)
-	    {
-		    if (prev_dir[0] == '\0')
-		    {
-			    printf("Previous directory not available.\n");
-			    return (1);
-		    }
-		    path = prev_dir;
-	    }
-	    if (getcwd(cwd, sizeof(cwd)) == NULL)
-	    {
-		    perror("getcwd");
-		    return (1);
-	    }
-	    strcpy(prev_dir, cwd);
-
-	    if (chdir(path) != 0)
-	    {
-		    printf("Error: Directory not found.\n");
-		    return (1);
-	    }
-	    if (getcwd(cwd, sizeof(cwd)) != NULL)
-		    setenv("PWD", cwd, 1);
-
-	    else
-	    {
-		    perror("getcwd");
-		    return (1);
-	    }
-
-
+	if (path == NULL)
+	{
+		path = getenv("HOME");
+		if (path == NULL)
+		{
+			printf("HOME environment variable not set.\n");
+			return (1);
+		}
+	}
+	else if (strcmp(path, "-") == 0)
+	{
+		if (prev_dir[0] == '\0')
+		{
+			printf("Previous directory not available.\n");
+			return (1);
+		}
+		path = prev_dir;
+	}
+	if (getcwd(cwd, sizeof(cwd)) == NULL)
+	{
+		perror("getcwd");
+		return (1);
+	}
+	strcpy(prev_dir, cwd);
+	if (chdir(path) != 0)
+	{
+		printf("Error: Directory not found.\n");
+		return (1);
+	}
+	if (getcwd(cwd, sizeof(cwd)) != NULL)
+		setenv("PWD", cwd, 1);
+	else
+	{
+		perror("getcwd");
+	}
 	return (1);
 }
 
